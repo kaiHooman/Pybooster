d708ebb57564bf7959b639d7fde12bd9882f01bf
Devyn Collier Johnson <DevynCJohnson@Gmail.com>
2016-03-17 18:09:30 -0400

    Delete BUILD_ALL.sh

--- BUILD_ALL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# -*- coding: utf-8 -*-
-# vim:fileencoding=utf-8
-# Created by Devyn Collier Johnson
-# <DevynCJohnson@Gmail.com>
-
-
-gnome-terminal --hide-menubar --title=Build_Pybooster -e './build.sh'
-exit 0
